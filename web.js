16d3026bee5bc8c1872a13dd51d41270d1be9be2
13ruce1337 <tagcoop@gmail.com>
2014-04-14 21:14:16 -0400

    server update

--- web.js
+++ web.js
@@ -1,7 +1,11 @@
-//Epiblog webserver initializer
+/* 
+	Epiblog webserver initializer
+*/
+
 var express = require("express");
 var logfmt = require("logfmt");
-var app = express();
+
+var app = module.exports = express();
 var port = Number(process.env.PORT || 5000);
 
 app.use(logfmt.requestLogger());
